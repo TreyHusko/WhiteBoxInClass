a24941281e430c94cc35d3a385ba7b5f951ee892
Trey <trey.husko@gmail.com>
2021-05-22 21:13:46 -0700

    modified tests

--- test_leapYear.py
+++ test_leapYear.py
@@ -3,20 +3,20 @@ import leapYear
 
 class testCaseLeapYear(unittest.TestCase):
     def test_LeapYear(self):
-        result =  leapYear.isLeapYear(2000)
-        self.assertEqual(result, True)
+        # result =  leapYear.isLeapYear(2000)
+        self.assertEqual(leapYear.isLeapYear(2000), True)
     def test_LeapYear2(self):
-        result =  leapYear.isLeapYear(1700)
-        self.assertEqual(result, False)
+        # result =  leapYear.isLeapYear(1700)
+        self.assertEqual(leapYear.isLeapYear(1700), False)
     def test_LeapYear3(self):
-        result =  leapYear.isLeapYear(1900)
-        self.assertEqual(result, True)
+        # result =  leapYear.isLeapYear(1900)
+        self.assertEqual(leapYear.isLeapYear(1900), True)
     def test_LeapYear4(self):
-        result =  leapYear.isLeapYear(1600)
-        self.assertEqual(result, False)
+        # result =  leapYear.isLeapYear(1600)
+        self.assertEqual(leapYear.isLeapYear(1600), False)
     def test_LeapYear6(self):
-        result =  leapYear.isLeapYear(2020)
-        self.assertEqual(result, True)
+        # result =  leapYear.isLeapYear(2020)
+        self.assertEqual(leapYear.isLeapYear(2020), True)
 
 
 if __name__ == "__main__":
